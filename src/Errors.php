480f7ea3254bf67d57257dd5ca375faca520aa59
VladKopalov <vladkop@mail.ru>
2021-06-13 02:16:38 +0300

    add support php 8.0

--- src/Errors.php
+++ src/Errors.php
@@ -16,30 +16,33 @@ class Errors
      */
     public static function attachExceptionHandler(array $custom = []): void
     {
-        set_exception_handler(static function (\Throwable $exception) use ($custom) {
-            http_response_code(550);
-            if (error_reporting() === E_ALL) {
-                throw $exception;
+        set_exception_handler(
+            static function (\Throwable $exception) use ($custom) {
+                http_response_code(550);
+                if (error_reporting() === E_ALL) {
+                    throw $exception;
+                }
+                $json = json_encode([
+                    'msg' => $exception->getMessage(),
+                    'query' => $_SERVER['QUERY_STRING'],
+                    'method' => $_SERVER['REQUEST_METHOD'],
+                    'trace' => $exception->getTrace(),
+                    'line' => $exception->getLine(),
+                    'file' => $exception->getFile(),
+                    'custom' => $custom,
+                ], JSON_THROW_ON_ERROR);
+                error_log($json);
+                Response::flush(Response::jsonError($exception->getMessage(), 'uncaught-exception'));
+                exit;
             }
-            $json = json_encode([
-                'msg' => $exception->getMessage(),
-                'query' => $_SERVER['QUERY_STRING'],
-                'method' => $_SERVER['REQUEST_METHOD'],
-                'trace' => $exception->getTrace(),
-                'line' => $exception->getLine(),
-                'file' => $exception->getFile(),
-                'custom' => $custom,
-            ], JSON_THROW_ON_ERROR);
-            error_log($json);
-            Response::flush(Response::jsonError($exception->getMessage(), 'uncaught-exception'));
-            exit;
-        });
+        );
     }
 
     /**
      * Завершает программу со ШТАТНОЙ ошибкой.
      *
-     * @param bool $condition Ошибка или нет
+     * @param bool $condition Ошибка
+     *                          или нет
      * @param string $error Сообщение
      * @param string $hint Подсказка
      *
